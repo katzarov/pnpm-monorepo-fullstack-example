361b9a4863a7cd84c0267408f68e4a95a9d77405
Lyoubomir Katzarov <lyoubomir.katzarov@gmail.com>
2026-04-21 13:05:42 +0300

    apigw + lambda + localsteck quick test

--- apps/iac/lib/iac-stack.ts
+++ apps/iac/lib/iac-stack.ts
@@ -1,16 +1,39 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+
+// import path from 'node:path';
+// import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class IacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'IacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // const fn = new NodejsFunction(this, 'MyFunction', {
+    //   runtime: lambda.Runtime.NODEJS_22_X,
+    //   handler: 'index.handler',
+    //   code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
+    //   bundling: {
+    //   }
     // });
+
+    const fn = new lambda.Function(this, 'HelloLambda', {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async () => {
+          return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "hello from lambda" })
+          };
+        };
+      `),
+    });
+
+    new apigateway.LambdaRestApi(this, 'HelloApi', {
+      handler: fn,
+    });
+
   }
 }
